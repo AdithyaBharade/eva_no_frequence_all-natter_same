8737ee278b034e64ecf42885e53704eec7d0f13b
Adithya.M.bharade <bharadeadithya@gmail.com>
2024-06-18 09:10:55 +0530

    Add files via upload

---
+++ eva_no_frequence_all natter_same.py
@@ -0,0 +1,26 @@
+'''
+Eva has a string S containing lowercase English letters. She wants to transform this 
+string into a Magic String, where all the characters in the string are the same. To do 
+so, she can replace any letter in the string with another letter present in that string. 6
+Your task is to help Eva find and return an integer value, representing the minimum 
+number of steps required to form a Magic String. Return 0, if S is already a Magic 
+String.
+Input Specification:
+input1: A string S, containing lowercase English letters.
+Output Specification:
+Return an integer value, representing the minimum number of steps required to form 
+a Magic String. Return 0, if S is already a Magic String.
+Sample Input:
+aaabbbccdddd
+Sample Output:
+8
+'''
+
+from collections import Counter
+def min_magicstring(s):
+    frequency=Counter(s)
+    max_freq=max(frequency.values())
+    min_step=len(s)-max_freq
+    return min_step
+s='aaabbbccccccddddddd'
+print(min_magicstring(s))
